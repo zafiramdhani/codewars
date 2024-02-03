ef3d9ce3a056057e36c4994bf6178b43f8985e70
zafiramdhani <izzafiramdhanialkaff@gmail.com>
2024-02-03 21:21:56 +0700

    Update challenge description

--- 8-kyu/counting-sheep/counting-sheep.js
+++ 8-kyu/counting-sheep/counting-sheep.js
@@ -1,4 +1,5 @@
 /* 
+TITLE = Counting Sheep
 URL = https://www.codewars.com/kata/54edbc7200b811e956000556/javascript
 DESC = Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
 */
