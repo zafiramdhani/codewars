72a0c403e689a04de23e5ba96a3f465d7172e2d7
zafiramdhani <izzafiramdhanialkaff@gmail.com>
2024-02-03 21:18:46 +0700

    Update challenge description

--- 8-kyu/counting-sheep/counting-sheep.js
+++ 8-kyu/counting-sheep/counting-sheep.js
@@ -1,3 +1,9 @@
+/* 
+URL = https://www.codewars.com/kata/54edbc7200b811e956000556/javascript
+DESC = Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+*/
+
+// CODE STARTS HERE
 function countSheeps(sheep) {
   const result = sheep.filter(v => v === true).length;
   return result;
